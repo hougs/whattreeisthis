0194f01996ee5001edc33792cb01586a1501584a
Juliet Hougland <not@myemail.com>
2019-05-14 11:30:53 -0700

    Dynamic data loading

--- src/App.js
+++ src/App.js
@@ -17,27 +17,69 @@ class App extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const currentQuestion = this.getByNodeId(history[this.state.stepNumber].nodeId);
+
+    this.setState({
+      history: history.concat([{
+        nodeId: currentQuestion.possible_answers[i].next_q
+      }
+    ]),
+      stepNumber: history.length
+    });
+  }
+
+  getByNodeId(nodeId) {
+    return( questionData.filter(function(item){
+        return item.id===nodeId;
+    })[0]);
+  }
+
   render() {
       const history = this.state.history;
-      const currentNodeId = history[this.state.stepNumber].nodeId;
-      const currentQuestion = questionData.filter(function(item){
-          return item.id===0;
-      })[0];
-
+      const currentQuestion = this.getByNodeId(history[this.state.stepNumber].nodeId);
+      console.log("history is " + JSON.stringify(this.state.history));
       return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {currentQuestion.question}
-          </p>
-          {currentQuestion.possible_answers.map((item, i) => (
-            <button>{item.text}</button>)
-    )}
+
         </header>
+        <Question
+        question_text={currentQuestion.question}
+        possible_answers={currentQuestion.possible_answers}
+        onClick={i => this.handleClick(i)}/>
       </div>
     );
   }
 }
+
+class Question extends React.Component {
+  renderQuestionResponse(i) {
+    return (
+      <button
+      className="QuestionResponse"
+      onClick={() => this.props.onClick(i)}
+      key={this.props.possible_answers[i].text}>
+        {this.props.possible_answers[i].text}
+      </button>
+    );
+  }
+
+  render() {
+    return(
+      <div>
+        <div className="question-text">
+          {this.props.question_text}
+        </div>
+        {this.props.possible_answers.map((item, i) => (
+          this.renderQuestionResponse(i))
+        )}
+      </div>)
+  }
+
+}
+
 
 export default App;
