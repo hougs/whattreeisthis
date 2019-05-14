b4860e22c458bc2a8a5f0b69ebf18b1f2432fe74
Juliet Hougland <not@myemail.com>
2019-05-14 10:21:36 -0700

    Basic app that renders a question and buttons.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const questionData = require('./questionData.json');
+console.log(questionData);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [
+        {
+          nodeId: 0
+        }
+      ],
+      stepNumber: 0
+    };
+  }
+
+  render() {
+      const history = this.state.history;
+      const currentNodeId = history[this.state.stepNumber].nodeId;
+      const currentQuestion = questionData.filter(function(item){
+          return item.id===0;
+      })[0];
+
+      return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {currentQuestion.question}
+          </p>
+          {currentQuestion.possible_answers.map((item, i) => (
+            <button>{item.text}</button>)
+    )}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
